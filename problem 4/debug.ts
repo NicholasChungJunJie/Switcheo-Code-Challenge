45a15f55a5df55f6a681a336a7a7d4fbb24f685b
Nicholas Chung <84615041+NicholasChungJunJie@users.noreply.github.com>
2022-12-13 23:02:55 +0800

    Update debug.ts

--- problem 4/debug.ts
+++ problem 4/debug.ts
@@ -5,7 +5,8 @@ import { AbiCoder } from "@ethersproject/abi";
 import { BigNumber, ethers, providers, Signer } from "ethers";
 
 // ethers.getDefaultProvider("0xc0ecb8499d8da2771abcbf4091db7f65158f1468");
-const provider = new ethers.providers.EtherscanProvider();
+const provider =  ethers.getDefaultProvider();
+// console.log(provider.getCode("0xc0ecb8499d8da2771abcbf4091db7f65158f1468"));
 const abi = [
     // Read-Only Functions
     "function balanceOf(address owner) view returns (uint256)",
@@ -32,7 +33,7 @@ async function get_amount ( address:string ) {
         console.log(e);
     }
 }
-
+// get_amount("0xc0ecb8499d8da2771abcbf4091db7f65158f1468");
 get_amount("0xb5d4f343412dc8efb6ff599d790074d0f1e8d430");
 get_amount("0x0020c5222a24e4a96b720c06b803fb8d34adc0af");
 get_amount("0xd1d8b2aae2ebb2acf013b803bc3c24ca1303a392");
